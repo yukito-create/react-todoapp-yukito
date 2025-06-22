37b563de8f9727c798f67e2715eda016086ca970
yukito <yukito@inoueshoutonoMacBook-Air.local>
2025-06-22 13:24:15 +0900

    inputForm added.

---
+++ src/components/InputForm.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const InputForm = () => {
+  return (
+    <div className="inputForm">
+      <form>
+        <input type="text" />
+        <button><i class="fa-solid fa-square-plus"></i></button>
+      </form>
+    </div>
+  )
+}
