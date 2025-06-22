d107db59e8572af2a3e26b247d051e990f441c1a
yukito <yukito@inoueshoutonoMacBook-Air.local>
2025-06-22 16:54:53 +0900

    addTodo completed.

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,23 +1,25 @@
 import React from 'react'
 
-export const TodoList = () => {
+export const TodoList = ({ taskList, setTaskList }) => {
   return (
     <div className="todoList">
       <div className="todos">
+        {taskList.map((task, index) => (
+          <div className="todo" key={index}>
+            <div className="todoText">
+              <span>{task.text}</span>
+            </div>
+            <div className="icons">
+              <button>
+              <i className="fa-solid fa-check"></i>
+              </button>
+              <button>
+              <i className="fa-solid fa-trash"></i>
+              </button>
+            </div>
+          </div>
+        ))}
         {/* 個別のTodo要素 */}
-        <div className="todo">
-          <div className="todoText">
-            <span>プログラミング</span>
-          </div>
-          <div className="icons">
-            <button>
-            <i class="fa-solid fa-check"></i>
-            </button>
-            <button>
-            <i class="fa-solid fa-trash"></i>
-            </button>
-          </div>
-        </div>
         {/* 個別のTodo要素 */}
       </div>
     </div>
